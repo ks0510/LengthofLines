c6efe2b35a731f14f152778be31dcdecd3c1efa5
ks0510 <kshaikh051999@gmail.com>
2023-04-08 21:58:32 +0530

    Added welcome messege program

---
+++ DisplayMessege.java
@@ -0,0 +1,14 @@
+package linegeometry;
+/**
+ * 
+ * @author Kaif
+ *
+ */
+
+public class DisplayMessege {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program");
+  }
+}
